725ea8443d3e4abd243d1528eb530058a7994d9f
kamimura <baipaa0723@gmail.com>
2018-03-01 12:01:59 +0900

    JSONファイルをtodo.jsonに保存するところで詰まっている

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
+"use strict";
+exports.__esModule = true;
 var arg = process.argv;
+var fs = require("fs");
+var MyTodo = /** @class */ (function () {
+    function MyTodo(content, id) {
+        this.content = content;
+        this.id = id;
+    }
+    return MyTodo;
+}());
 var TodoManager = /** @class */ (function () {
-    function TodoManager(str) {
-        this.str = str;
-        if (str == "") {
-            this.getTodo();
+    function TodoManager(args) {
+        this.args = args;
+        //this.myTodo = this.getTodo();
+        this.myTodo = this.getTodo();
+        if (args.length == 2) {
+            this.displayTodo(this.myTodo);
         }
-        else if (str == "add") {
-            this.addTodo();
+        else if (args[2] == "add") {
+            this.addTodo(args[3], this.myTodo); //args[3]のTODOを登録する
         }
-        else if (str == "complete") {
-            this.completeTodo();
+        else if (args[2] == "complete") {
+            this.completeTodo(args[3], this.myTodo); //args[3]のTODOを完了する
         }
     }
     TodoManager.prototype.getTodo = function () {
+        //const str = fs.readFileSync('todo.txt','utf8');
+        //JSONから文字列リスト型に変換する
+        var myTodo = new Array();
+        myTodo.push(new MyTodo("hoge", 1));
+        myTodo.push(new MyTodo("fuga", 2));
+        return myTodo;
     };
-    TodoManager.prototype.addTodo = function () {
+    TodoManager.prototype.displayTodo = function (myTodo) {
+        console.log(myTodo);
     };
-    TodoManager.prototype.completeTodo = function () {
+    TodoManager.prototype.addTodo = function (arg, myTodo) {
+        //myTodoにnewTodoを付け加える
+        var newTodo = new MyTodo(arg, this.produceID());
+        myTodo.push(newTodo);
+        //myTodoをJSONに直して保存
+        var todo = "{\n            \"TODO\":[\n                {\n                    \"id\":\"1\",\n                    \"todo\":\"\u722A\u3092\u5207\u308B\"\n                },\n                {\n                    \"id\":\"2\",\n                    \"todo\":\"\u98A8\u5442\u306B\u5165\u308B\"\n                }\n            ]\n        }";
+        var jsonFile = JSON.parse(todo);
+        fs.writeFileSync('todo.txt', jsonFile, 'utf8');
+    };
+    TodoManager.prototype.completeTodo = function (id_str, myTodo) {
+        var id = parseInt(id_str);
+    };
+    TodoManager.prototype.produceID = function () {
+        return 5;
     };
     return TodoManager;
 }());
-if (process.argv.length >= 0) {
-    console.log(arg[2]);
-}
+var todoManager = new TodoManager(arg);
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 const arg = process.argv
+import fs = require('fs');
 
 interface Todo{
     content:string;
@@ -8,41 +9,71 @@ interface Todo{
 class MyTodo implements Todo{
     content:string;
     id:number;
-    str:string;
+    constructor(content:string,id:number){
+        this.content = content;
+        this.id = id;
+    }
 }
 
 class TodoManager{
-    private myTodo:MyTodo[];
-    let myTodo = this.getTodo();
+    private myTodo:Array<MyTodo>;
     constructor(public args: string[]){
+        //this.myTodo = this.getTodo();
+        this.myTodo = this.getTodo();
         if(args.length == 2){
-            this.displayTodo();
+            this.displayTodo(this.myTodo);
         }else if(args[2] == "add"){
-            this.addTodo(args[3]);  //args[3]のTODOを登録する
+            this.addTodo(args[3],this.myTodo);  //args[3]のTODOを登録する
         }else if(args[2] == "complete"){
-            this.completeTodo(args[3]); //args[3]のTODOを完了する
+            this.completeTodo(args[3],this.myTodo); //args[3]のTODOを完了する
         }
     }
 
     private getTodo(){  //TODOリストを取得
-        import fs = require('fs');
-        const str = fs.readFileSync('todo.txt','utf8');
-
-        return str;
+        //const str = fs.readFileSync('todo.txt','utf8');
+        //JSONから文字列リスト型に変換する
+        const myTodo = new Array<MyTodo>();
+        myTodo.push(new MyTodo("hoge",1));
+        myTodo.push(new MyTodo("fuga",2));
+        return myTodo;
     }
 
-    private displayTodo(){
-
+    private displayTodo(myTodo:Array<MyTodo>){
+        
+        console.log(myTodo);
     }
 
-    private addTodo(todo:string){  //TODOを加える
-        const id = 
+    private addTodo(arg:string,myTodo:Array<MyTodo>){  //TODOを加える
+        //myTodoにnewTodoを付け加える
+        const newTodo = new MyTodo(arg,this.produceID());
+        myTodo.push(newTodo);
+        //myTodoをJSONに直して保存
+        const todo = `{
+            "TODO":[
+                {
+                    "id":"1",
+                    "todo":"爪を切る"
+                },
+                {
+                    "id":"2",
+                    "todo":"風呂に入る"
+                }
+            ]
+        }`;
+        const jsonFile = JSON.parse(todo);
+        fs.writeFileSync('todo.txt',jsonFile,'utf8');
     }
 
-    private completeTodo(id_str:string){
+    private completeTodo(id_str:string,myTodo:Array<MyTodo>){
         const id = parseInt(id_str);
 
     }
+
+    private produceID(){
+        return 5;
+    }
 }
 
+
+const todoManager = new TodoManager(arg);
 
